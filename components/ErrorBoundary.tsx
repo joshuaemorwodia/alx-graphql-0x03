0987d242eaad25cb90bb073f00c8c2d22fac6430
Joshua Emorwodia <joshuaemorwodia@gmail.com>
2025-07-03 05:16:19 +0100

    Ensure ErrorBoundary.tsx is populated and committed

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,3 +1,4 @@
+// components/ErrorBoundary.tsx
 import React, { ReactNode } from "react";
 
 interface State {
@@ -19,7 +20,7 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log("Error caught by ErrorBoundary:", { error, errorInfo });
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
   }
 
   render() {
